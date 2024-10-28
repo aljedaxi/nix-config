29d06d55b72e450356d62fbd508df14ecd4a8b5b
Kathy Acker <aljedaxi@pm.me>
2024-10-28 00:34:09 -0600

    feat: idk i forgor

--- configuration.nix
+++ configuration.nix
@@ -22,10 +22,6 @@
   # Set your time zone.
   time.timeZone = "US/Mountain";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
@@ -53,21 +49,18 @@
 	};
 	services.picom.enable = true;
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  hardware.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
+	environment.shells = with pkgs; [ zsh ];
+	users.defaultUserShell = pkgs.zsh;
+	programs.zsh.enable = true;
   users.users.daxi = {
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
@@ -75,6 +68,7 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
+	nixpkgs.config.permittedInsecurePackages = [ "electron-27.3.11" ]; # needed for logseq >~<
   environment.systemPackages = with pkgs; [
 		hsetroot
 		(st.overrideAttrs (oldAttrs: rec {
@@ -85,7 +79,10 @@
 		firefox
 		dmenu
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
+    telegram-desktop
+		dunst
+		logseq
+		pulseaudio
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -100,12 +97,6 @@
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
